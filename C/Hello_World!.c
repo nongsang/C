88c5170714e1583d9000d743f34cdb0bbfd5a23f
nongsang <shdtkd37@gmail.com>
2018-08-10 00:46:20 +0900

    8월 10일 hello world!

---
+++ C/Hello_World!.c
@@ -0,0 +1,7 @@
+#include <stdio.h>	// stdio.h를 포함하여 사용하고자 하는 함수가 있는 라이브러리를 명시
+
+int main(void)		// main()에서 실행한다.
+{
+	printf("hello, world!\n");	// 화면에 hello, world!라고 출력한다.
+	return 0;					// 0을 반환하여 종료함을 알린다.
+}
