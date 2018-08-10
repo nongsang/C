a15906eb3acbbb024f1adeef63d611bbc3731142
nongsang <shdtkd37@gmail.com>
2018-08-10 21:18:59 +0900

    8월 10일 삼항연산자

---
+++ C/009.Ternary_Operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 2, num2 = 3;
+	int result;
+
+	result = (num1 > num2) ? num1 : num2;	// 조건이 true면 num1을, false면 num2을 result에 저장한다.
+	printf("result : %d\n", result);
+}
